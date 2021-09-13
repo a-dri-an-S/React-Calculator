b44bcbb17b63a1ea09d19a9287d9a1e46240044b
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-09-12 18:01:11 -0700

    added button components to Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,44 @@
+import NumberButton from './NumberButton';
+import FunctionButton from './FunctionButton';
+import ClearButton from './ClearButton';
+import Display from './Display';
+import EqualButton from './EqualButton';
+import BackButton from './BackButton';
+import NegativeButton from './NegativeButton';
+import { CalculatorStyles } from './styles/Styles';
+
+const Calculator = () => {
+    return ( 
+        <CalculatorStyles>
+            <div className="display">
+                <h1>Lemme get yo' digitz</h1>
+                <Display />
+            </div>
+            <div className="number-pad">
+                <ClearButton />
+                <BackButton />
+                <NegativeButton />
+                <FunctionButton btnVal="/" />
+                <NumberButton btnVal={ 7 } />
+                <NumberButton btnVal={ 8 } />
+                <NumberButton btnVal={ 9 } />
+                <FunctionButton btnVal="*" />
+                <NumberButton btnVal={ 4 } />
+                <NumberButton btnVal={ 5 } />
+                <NumberButton btnVal={ 6 } />
+                <FunctionButton btnVal="-" />
+                <NumberButton btnVal={ 1 } />
+                <NumberButton btnVal={ 2 } />
+                <NumberButton btnVal={ 3 } />
+                <FunctionButton btnVal="+" />
+                <div>
+                    <NumberButton btnVal={ 0 } />
+                </div>
+                <NumberButton btnVal="." />
+                <EqualButton />
+            </div>
+        </CalculatorStyles>
+    );
+}
+
+export default Calculator;
